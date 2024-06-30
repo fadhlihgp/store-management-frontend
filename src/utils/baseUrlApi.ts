41eaca044e682a04242196add4161ff94117dcc3
fadhlihgp <fadhlihgp@gmail.com>
2024-06-30 23:37:49 +0700

    init

--- src/utils/baseUrlApi.ts
+++ src/utils/baseUrlApi.ts
@@ -1,1 +1,2 @@
-export const baseUrlApi = "https://localhost:7203/gateway";
+// export const baseUrlApi = "https://localhost:7203/gateway";
+export const baseUrlApi = "http://139.99.84.202:1011/gateway";
